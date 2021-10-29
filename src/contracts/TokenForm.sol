53e574a668c51daafe45ffd962a5d15b4512b6e2
Sachin <sachinkshuklaoo7@gmail.com>
2021-10-29 09:17:05 +0530

    tranfer tokens success

--- src/contracts/TokenForm.sol
+++ src/contracts/TokenForm.sol
@@ -1,5 +1,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
